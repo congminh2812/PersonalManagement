27d8cdcb22a555a4341fe6c262d2f770d3dea9da
congminh2812 <congminh281297@gmail.com>
2023-05-26 16:17:36 +0700

    add source client

---
+++ PersonalManagement/personalmanagement.client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './layouts/dash-board';
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
